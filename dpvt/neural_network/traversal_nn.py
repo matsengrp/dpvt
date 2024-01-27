d8fc39ec0e2ec771ed9637e342fc016e902ca0e6
Harry Richman <dhrichman@gmail.com>
2024-01-26 16:16:28 -0800

    add neural_network files

---
+++ dpvt/neural_network/traversal_nn.py
@@ -0,0 +1,36 @@
+from torch import nn
+from ete3 import Tree
+
+# tree with maximum parsimony
+t_good = Tree("(0,(1,2));")
+for node in (t_good, t_good.children[0]):
+    node.sequence = "A"
+for node in (t_good.children[1], t_good.children[1].children[0], t_good.children[1].children[1]):
+    node.sequence = "T"
+print(t_good.get_ascii(attributes=["sequence"]))
+
+# tree without maximum parsimony
+t_bad = Tree("(0,(1,2));")
+for node in (t_bad, t_bad.children[1], t_bad.children[1].children[1]):
+    node.sequence = "A"
+for node in (t_bad.children[0], t_bad.children[1].children[0]):
+    node.sequence = "T"
+print(t_bad.get_ascii(attributes=["sequence"]))
+
+class TraverseNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(8, 32),
+            nn.ReLU(),
+            nn.Linear(32,4),
+        )
+        self.down_traverse_stack = nn.Sequential(
+            nn.Linear(8, 32),
+            nn.ReLU(),
+            nn.Linear(32, 4),
+        )
+
+    def forward(self, x):
+        logits = self.linear_relu_stack(x)
+        return logits
