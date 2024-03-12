b4a9d616941d38b8a27eba57f6006e5e70438356
Harry Richman <dhrichman@gmail.com>
2024-03-12 11:41:40 -0700

    add test

---
+++ tests/test_traverse_nn.py
@@ -0,0 +1,9 @@
+from dpvt.neural_network.traverse_nn import TraverseNN
+from dpvt.neural_network.training_data import good_trees
+
+def test_nn():
+    tnn = TraverseNN()
+    tree = good_trees[0]
+    print(tree.get_ascii(attributes=["sequence", "to_parent"]))
+    out = tnn(tree)
+    assert out > 0
