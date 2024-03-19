067395d3f2d7f121712f07fbbfe77c6738a01e34
Harry Richman <dhrichman@gmail.com>
2024-03-19 12:04:51 -0700

    update test

--- tests/test_traverse_nn.py
+++ tests/test_traverse_nn.py
@@ -1,9 +1,13 @@
 from dpvt.neural_network.traverse_nn import TraverseNN
-from dpvt.neural_network.training_data import good_trees
+from dpvt.neural_network.training_data import (
+    good_trees,
+    site4_good_trees,
+)
 
 def test_nn():
     tnn = TraverseNN()
-    tree = good_trees[0]
+    # tree = good_trees[0]
+    tree = site4_good_trees[0]
     print(tree.get_ascii(attributes=["sequence", "to_parent"]))
-    out = tnn(tree)
+    out = tnn([tree])
     assert out > 0
