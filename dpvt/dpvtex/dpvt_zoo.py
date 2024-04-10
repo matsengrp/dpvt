ef87a045ef82d0f42c24325c22360a841fd677c0
lenacoll <lena@lenacoll.de>
2024-04-10 10:45:35 -0700

    add hyperparameter optimization + early stopping

--- dpvt/dpvtex/dpvt_zoo.py
+++ dpvt/dpvtex/dpvt_zoo.py
@@ -1,13 +1,13 @@
 from neural_network import models
-from neural_network.wrapper import Wrap
+from neural_network.wrapper import Wrap, HyperWrap
 from dpvtex.dpvt_data import train_val_data_of_nicknames
 
 
-def create_model(model_name, learning_rate):
+def create_model(model_name):
     if model_name == "traverseNN":
-        model = models.TraverseNN(learning_rate)
+        model = models.TraverseNN
     elif model_name == "TransformerEncoderTraversal":
-        model = models.TransformerEncoderTraversal(learning_rate)
+        model = models.TransformerEncoderTraversal
     return model
 
 
@@ -19,16 +19,31 @@ def trained_model_path(model_name, data_name):
     return f"trained_models/{trained_model_str(model_name, data_name)}"
 
 
+def best_model_params_path(model_name, data_name):
+    return f"hyper_checkpoints/{trained_model_str(model_name, data_name)}"
+
+
 def train_model(model_name, data_name, final_checkpoint, **wrap_kwargs):
-    # hyperparameter
-    learning_rate = 0.01
-    # model parameters
-    default_params = {"batch_size": 1024, "epochs": 200}
+    # hyperparameters (only used if no hyperparameter testing done)
+    default_params = {
+        "learning_rate": 0.01,
+        "batch_size": 1024,
+        "epochs": 200,
+    }
     # Update default parameters with any provided keyword arguments
     wrap_params = {**default_params, **wrap_kwargs}
     train_data, val_data = train_val_data_of_nicknames(data_name)
-    model = create_model(model_name, learning_rate)
+    model = create_model(model_name)
     model_str = trained_model_str(model_name, data_name)
     wrap = Wrap(train_data, val_data, model, model_str, **wrap_params)
     wrap.train(final_checkpoint)
+    return model
+
+
+def optimize_hyperparameters(model_name, data_name, best_model_hparams_filepath):
+    train_data, val_data = train_val_data_of_nicknames(data_name)
+    model = create_model(model_name)
+    model_str = trained_model_str(model_name, data_name)
+    hyper_wrap = HyperWrap(model, train_data, val_data, model_str, 10)
+    hyper_wrap.optuna_optimize(best_model_hparams_filepath)
     return model
