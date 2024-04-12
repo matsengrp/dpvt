9360bddd6f48d8013e2725ef1e0413fb1ad8cd2d
lenacoll <lena@lenacoll.de>
2024-04-12 08:32:55 -0700

    updata data generation: Save dict of all data (training+validation+testing) as one dict with labels 0/1

--- dpvt/generate_data/generate_train_test_sets.py
+++ dpvt/generate_data/generate_train_test_sets.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from pathlib import Path
-import torch
 import pickle
 
 sys.path.append("..")
@@ -16,25 +15,11 @@ from generate_data.training_data import (
 
 def create_training_data(file_path, good_trees, bad_trees):
 
-    tree_to_label = {
+    data_dict = {
         **{tree: 0.0 for tree in good_trees},
         **{tree: 1.0 for tree in bad_trees},
     }
-    # Calculate the number of items for training and validation
-    num_items = len(tree_to_label)
-    num_train = int(num_items * 0.8)
-
-    # Shuffle the keys and split them
-    keys = list(tree_to_label.keys())
-    random_idx = torch.randperm(num_items)
-    train_keys = [keys[i] for i in random_idx[:num_train]]
-    val_keys = [keys[i] for i in random_idx[num_train:]]
-
-    # Create subsets based on the split keys
-    train_data = {key: tree_to_label[key] for key in train_keys}
-    val_data = {key: tree_to_label[key] for key in val_keys}
-
-    data_dict = {"train": train_data, "val": val_data}
+    # data_dict = {"train": train_data, "val": val_data}
     with open(file_path, "wb") as f:
         pickle.dump(data_dict, file=f)
 
