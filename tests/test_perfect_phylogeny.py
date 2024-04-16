3a5f3f74a0928e6d5471cbe0b126cfb8deab5a18
Harry Richman <dhrichman@gmail.com>
2024-04-16 15:42:51 -0700

    update test with new keyword arguments

--- tests/test_perfect_phylogeny.py
+++ tests/test_perfect_phylogeny.py
@@ -8,14 +8,14 @@ def test_3_leaf_1_site():
     tree_gen = pp.make_phylogenies(
         use_seq=True,
         use_sub=False,
-        unique_leaves=True,
-        sub_on_all_internal=True,
+        # unique_leaves=True,
+        sub_on_all_edges=False,
         max_sites=1,
     )
     tree_list = list(tree_gen)
-    assert len(tree_list) == 120
+    assert len(tree_list) == 60
     expected_newick = (
-        "((0[&&NHX:sequence=C],1[&&NHX:sequence=G])[&&NHX:sequence=G],2[&&NHX:sequence="
+        "((0[&&NHX:sequence=G],1[&&NHX:sequence=G])[&&NHX:sequence=G],2[&&NHX:sequence="
         "A]);"
     )
     assert tree_list[0].write(format=9, features=["sequence"]) == expected_newick
@@ -27,12 +27,12 @@ def test_5_leaf_2_sites():
     tree_gen = pp.make_phylogenies(
         use_seq=False,
         use_sub=True,
-        unique_leaves=True,
-        sub_on_all_internal=True,
+        # unique_leaves=True,
+        sub_on_all_edges=False,
         max_sites=2,
     )
     expected_newick = (
-        "(0[&&NHX:subs={}],(1[&&NHX:subs={}],((2[&&NHX:subs={C1T}],3[&&NHX:subs={}])[&&"
+        "(0[&&NHX:subs={}],(1[&&NHX:subs={}],((2[&&NHX:subs={}],3[&&NHX:subs={}])[&&"
         "NHX:subs={G1C}],4[&&NHX:subs={}])[&&NHX:subs={A1G}])[&&NHX:subs={A0G}]);"
     )
     assert next(tree_gen).write(format=9, features=["subs"]) == expected_newick
