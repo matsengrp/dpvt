c30d2941be80bfcb4a0a5b2ebc415d21c726315e
Harry Richman <dhrichman@gmail.com>
2024-06-13 11:00:47 -0700

    refactor creating of perturbed perfect phylogenies

--- examples/generating_perfect_phylogenies/make_datasets.py
+++ examples/generating_perfect_phylogenies/make_datasets.py
@@ -2,24 +2,24 @@ import pickle
 import torch
 from ete3 import Tree
 
-from dpvt.generate_data.utils import Tree as MyTree
+from dpvt.generate_data.utils import populate
 from dpvt.generate_data.perfect_phylogeny import PerfectPhylogeny
 from dpvt.generate_data.perturb_phylogeny import (
     perturb_tree,
     sankoff_for_missing_sequences,
 )
 
-def create_training_data(file_path, n_trees, n_leaves):
+def create_training_data(n_trees, n_leaves):
     """
-    Create a collection of phylogenies obtained by randomly mixing a subtree of a perfect
-    phylogeny.
+    Create a collection of phylogenies obtained by randomly mixing a subtree of a
+    perfect phylogeny.
     """
     DEPTH = 4
     n_phylos_per_tree = 32
     tree_data_dict = {}
     for _ in range(n_trees):
-        tree = MyTree()
-        tree.populate(n_leaves, model="uniform")
+        tree = Tree()
+        populate(tree, n_leaves, model="uniform")
         pp = PerfectPhylogeny(tree)
         for _ in range(n_phylos_per_tree):
             phylo = pp.make_random_phylogeny()
@@ -56,16 +56,17 @@ def create_training_data(file_path, n_trees, n_leaves):
     val_data = {key: tree_data_dict[key] for key in val_keys}
 
     data_dict = {"train": train_data, "val": val_data}
-    with open(file_path, "wb") as fh:
-        pickle.dump(data_dict, file=fh)
+    return data_dict
 
 N_LEAVES = 30
 def main():
-    create_training_data(
-        file_path=f"{N_LEAVES}leaf_perfect.p", 
+    data_dict = create_training_data(
         n_trees=32, 
         n_leaves=N_LEAVES
     )
+    file_path=f"{N_LEAVES}leaf_perfect.p"
+    with open(file_path, "wb") as fh:
+        pickle.dump(data_dict, file=fh)
 
 if __name__ == "__main__":
     main()
