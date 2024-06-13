4126b56444f65b3f44725b22d1dc2dd0233d79d0
Harry Richman <dhrichman@gmail.com>
2024-06-13 11:25:41 -0700

    remove unneeded buggy code; refactor

--- examples/generating_perfect_phylogenies/make_datasets.py
+++ examples/generating_perfect_phylogenies/make_datasets.py
@@ -9,29 +9,24 @@ from dpvt.generate_data.perturb_phylogeny import (
     sankoff_for_missing_sequences,
 )
 
-def create_training_data(n_trees, n_leaves):
+def create_training_data(n_trees, n_phylos_per_tree, n_leaves, tree_depth):
     """
     Create a collection of phylogenies obtained by randomly mixing a subtree of a
     perfect phylogeny.
     """
-    DEPTH = 4
-    n_phylos_per_tree = 32
     tree_data_dict = {}
     for _ in range(n_trees):
+        # create a random tree
         tree = Tree()
         populate(tree, n_leaves, model="uniform")
         pp = PerfectPhylogeny(tree)
         for _ in range(n_phylos_per_tree):
+            # create a random perfect phylogeny
             phylo = pp.make_random_phylogeny()
-            mixed_phylo = perturb_tree(phylo, depth=DEPTH, exception_on_fail=True)
+            mixed_phylo = perturb_tree(phylo, depth=tree_depth, exception_on_fail=True)
             # assert(mixed_phylo is not None)
+            # apply sankoff algorithm to get sequences on internal nodes
             sankoff_for_missing_sequences(mixed_phylo)
-            # convert custom Tree object to ete3 Tree
-            newick = mixed_phylo.write(
-                features=["sequence", "random_tree"], 
-                format_root_node=True
-            )
-            mixed_phylo = Tree(newick)
             # add "extra" unifurcating root above previous root
             new_tree = Tree()
             new_tree.add_child(mixed_phylo)
@@ -45,11 +40,14 @@ def create_training_data(n_trees, n_leaves):
             tree_data_dict[mixed_phylo] = edge_classifier
     return tree_data_dict
 
-N_LEAVES = 30
+N_LEAVES = 5
+DEPTH = 2
 def main():
     data_dict = create_training_data(
         n_trees=32, 
-        n_leaves=N_LEAVES
+        n_phylos_per_tree=32,
+        n_leaves=N_LEAVES,
+        tree_depth=DEPTH,
     )
     file_path=f"{N_LEAVES}leaf_perfect.p"
     with open(file_path, "wb") as fh:
